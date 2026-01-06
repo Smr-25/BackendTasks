be8b806ffb3b353d3f3dc6cbd9988df55acc5018
Smr-25 <samiraa@code.edu.az>
2026-01-06 20:32:05 +0400

    06.01.26 task v4

--- PustokApp/PustokApp/Controllers/HomeController.cs
+++ PustokApp/PustokApp/Controllers/HomeController.cs
@@ -1,7 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using PustokApp.Data;
+using PustokApp.ViewModels;
 
-namespace PustokApp.Controllers;
-
-public class HomeController : Controller
+namespace PustokApp.Controllers
 {
+    public class HomeController(AppDbContext dbContext) : Controller
+    {
+        public IActionResult Index()
+        {
+            HomeVm homeVm = new HomeVm
+            {
+                Sliders = dbContext.Sliders.ToList()
+            };
+            return View(homeVm);
+        }
+    }
 }
