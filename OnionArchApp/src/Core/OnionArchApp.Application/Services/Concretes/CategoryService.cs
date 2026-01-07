a7820a46cf57ac3d947627b035accdb6153f44bc
Smr-25 <samiraa@code.edu.az>
2026-01-07 23:54:29 +0400

    Restore OnionArchApp v5

--- OnionArchApp/src/Core/OnionArchApp.Application/Services/Concretes/CategoryService.cs
+++ OnionArchApp/src/Core/OnionArchApp.Application/Services/Concretes/CategoryService.cs
@@ -2,37 +2,37 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using OnionArchApp.Application.Dtos.Category;
 using OnionArchApp.Application.Interfaces;
+using OnionArchApp.Application.Models;
 using OnionArchApp.Application.Services.Interfaces;
 using OnionArchApp.Domain.Entities;
 
 namespace OnionArchApp.Application.Services.Concretes;
 
-public class CategoryService(IApplicationDbContext applicationDbContext,IMapper mapper) : ICategoryService
+public class CategoryService(IApplicationDbContext applicationDbContext, IMapper mapper) : ICategoryService
 {
-    public async Task<List<CategoryReturnDto>> GetAllCategoriesAsync()
+    public async Task<ResponseModel<List<CategoryReturnDto>>> GetAllCategoriesAsync()
     {
         var categories = await applicationDbContext.Categories.ToListAsync();
-        var categoryDtos = mapper.Map<List<CategoryReturnDto>>(categories);
-        return categoryDtos;
+        var categoryReturnDtos = mapper.Map<List<CategoryReturnDto>>(categories);
+        return ResponseModel<List<CategoryReturnDto>>.Success(categoryReturnDtos);
     }
 
-    public async Task CreateCategoryAsync(CategoryCreateDto categoryDto)
+    public async Task<ResponseModel<CategoryReturnDto>> CreateCategoryAsync(CategoryCreateDto categoryDto)
     {
         var category = mapper.Map<Category>(categoryDto);
-        await applicationDbContext.Categories.AddAsync(category);
-        await applicationDbContext.SaveChangesAsync();
+        applicationDbContext.Categories.AddAsync(category);
+        applicationDbContext.SaveChangesAsync();
+        var categoryReturnDto = mapper.Map<CategoryReturnDto>(category);
+        return ResponseModel<CategoryReturnDto>.Success(categoryReturnDto);
     }
 
-    public Task UpdateCategoryAsync(CategoryUpdateDto categoryDto)
-    {
-        var category = mapper.Map<Category>(categoryDto);
-        applicationDbContext.Categories.Update(category);
-        return applicationDbContext.SaveChangesAsync();
-    }
-
-    public Task DeleteCategoryAsync(int categoryId)
+    public async Task UpdateCategoryAsync(int categoryId, CategoryUpdateDto categoryDto)
     {
         throw new NotImplementedException();
     }
 
+    public async Task DeleteCategoryAsync(int categoryId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- OnionArchApp/src/Core/OnionArchApp.Application/Services/Interfaces/ICategoryService.cs
+++ OnionArchApp/src/Core/OnionArchApp.Application/Services/Interfaces/ICategoryService.cs
@@ -1,12 +1,13 @@
 using OnionArchApp.Application.Dtos.Category;
+using OnionArchApp.Application.Models;
 using OnionArchApp.Domain.Entities;
 
 namespace OnionArchApp.Application.Services.Interfaces;
 
 public interface ICategoryService
 {
-    Task<List<CategoryReturnDto>> GetAllCategoriesAsync();
-    Task CreateCategoryAsync(CategoryCreateDto categoryDto);
-    Task UpdateCategoryAsync(CategoryUpdateDto categoryDto);
+    Task<ResponseModel<List<CategoryReturnDto>>> GetAllCategoriesAsync();
+    Task<ResponseModel<CategoryReturnDto>> CreateCategoryAsync(CategoryCreateDto categoryDto);
+    Task UpdateCategoryAsync(int categoryId, CategoryUpdateDto categoryDto);
     Task DeleteCategoryAsync(int categoryId);
 }
