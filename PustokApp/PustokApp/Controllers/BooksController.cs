a96483910d2ba1b1a66989b3e618644c4257f311
Smr-25 <samiraa@code.edu.az>
2026-01-09 09:29:56 +0400

    09.01.26 task v2

--- PustokApp/PustokApp/Controllers/BooksController.cs
+++ PustokApp/PustokApp/Controllers/BooksController.cs
@@ -6,22 +6,43 @@ using PustokApp.ViewModels;
 namespace PustokApp.Controllers;
 
 public class BooksController(
-    AppDbContext dbContext
+    AppDbContext dbContext,
+    ILogger<BooksController> logger
 ) : Controller
 {
     public IActionResult Details(int id)
     {
         var book = dbContext.Books
             .Include(x => x.Author)
+            .Include(x => x.BookImages)
+            .Include(x => x.BookTags)
+            .ThenInclude(x => x.Tag)
             .FirstOrDefault(b => b.Id == id);
+
+
         BookVm bookVm = new()
         {
             Book = book,
             RelatedBooks = dbContext.Books
                 .Include(x => x.Author)
+                .Include(x => x.BookImages)
                 .Where(b => b.AuthorId == book.AuthorId && b.Id != book.Id)
+                .Take(4)
                 .ToList()
         };
         return View(bookVm);
+    }
+
+    public IActionResult BookModal(int id)
+    {
+        var book = dbContext.Books
+            .Include(x => x.Author)
+            .Include(x => x.BookImages)
+            .Include(x => x.BookTags)
+            .ThenInclude(x => x.Tag)
+            .FirstOrDefault(b => b.Id == id);
+        if (book == null)
+            return NotFound();
+        return PartialView("_BookModalPartial", book);
     }
 }
